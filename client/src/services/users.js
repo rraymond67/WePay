99880c428cedaa943524eabc71cb241a82b8c637
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-25 16:39:02 -0500

    items

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -13,3 +13,15 @@ export const registerUser = async (registerUser) => {
   api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`
   return resp.data.user
 }
+
+export const verifyUser = async () => {
+  const token = localStorage.getItem('authToken')
+  if (token) {
+    api.defaults.headers.common.authorization = `Bearer ${token}`
+    const resp = await api.get('/auth/verify')
+    return resp.data
+  }
+  return false
+}
+
+export const 
