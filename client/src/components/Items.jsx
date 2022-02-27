ecb1b60f37895ad0fb3c7576a4073370211a4260
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-27 17:22:22 -0500

    Item.jsx

--- client/src/components/Items.jsx
+++ client/src/components/Items.jsx
@@ -1,3 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+export default function Items(props) {
+  return (
+    <div>
+      {props.items.map(item => (
+        <link key={item.id} to={`/items/${item.id}`}>
+          <h4>{item.title}</h4>
+          <img src={item.img_url} />
+          {/* <p>{item.description}</p> */}
+        </link>
+        
+      ))}
+    </div>
+  )
+}
