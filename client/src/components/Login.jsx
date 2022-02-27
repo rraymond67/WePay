5e197af08cc155b63b1bf9f5059a6cec15f6fb29
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-27 15:10:12 -0500

    login

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../services/users";
+
+export default function Login(props) {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  return (
+    <form onSubmit={async (e) => {
+      e.preventDefault()
+      const user = { username, password }
+      const resp = await loginUser(user)
+    }}>
+
+    </form>
+
+
+
+
+  )
+}
