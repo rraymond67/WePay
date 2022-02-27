fb235a05a832ecdfe867165ffe6069801e9829c0
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-27 16:05:32 -0500

    Item create

--- client/src/components/ItemCreate.jsx
+++ client/src/components/ItemCreate.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 
 export default function ItemCreate(props) {
   const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
   const [price, setPrice] = useState(0)
-  const [description, setPrice] = useState('')
   const [img_url, setImg_url] = useState('')
   
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault()
+      const item = { title, description, price, img_url }
+    }}
+  )
 
 }
