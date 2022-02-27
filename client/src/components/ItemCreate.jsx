19d25a08cac84f101ba853e8cc049d91875fae08
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-27 15:54:54 -0500

    Item create

---
+++ client/src/components/ItemCreate.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default function ItemCreate(props) {
+  const [title, setTitle] = useState('')
+  const [price, setPrice] = useState(0)
+  const [description, setPrice] = useState('')
+  const [img_url, setImg_url] = useState('')
+  
+
+}
