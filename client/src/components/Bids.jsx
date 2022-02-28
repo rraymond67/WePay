75431ce3f38761c6c6bd6547af303bf29197a44e
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-27 22:01:12 -0500

    sign up

---
+++ client/src/components/Bids.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Bids(props) {
+  return (
+    <div>
+      {props.bids && 
+        props.bids.map(bid => (
+          <div key={bid.id}>
+            <h4>{bid.user.username} is biddind {bid.amount}</h4>
+
+            {props.currentUser?.id === bid.user_id ?
+                <>
+                  <button>Edit</button>
+                  <button onClick={() => props.handleBidDelete(bid.id)}>
+                    Delete
+                  </button>
+                </>
+                :
+                null
+            }
+          </div>
+        ))
+      }
+    </div>
+  )
+}
--- client/src/components/ItemDetail.jsx
+++ client/src/components/ItemDetail.jsx
@@ -2,3 +2,64 @@ import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { getItemBids, createBid, deleteBid } from "../services/bids";
 
+export default function ItemDetail(props) {
+  const { id } = useParams()
+  
+  const [item, setItem] = useState({})
+  const [bids, setBids] = useState([])
+  const [toggle, setToggle] = useState(true)
+
+  useEffect(() => {
+    const fetchItem = props.items.find(item => item.id === parseInt(id))
+    const fetchBids = async () => {
+      const bids = await getItemBids(id)
+      setBids(bids)
+    }
+    fetchBids()
+    setItem(fetchItem)
+  }, [id, props.item, toggle])
+
+  const handleBidCreate = async (formData) => {
+    await createBid(id, formData)
+    setToggle(previous => !previous)
+  }
+  const handleBidDelete = async (bid_id) => {
+    await deleteBid(id, bid_id)
+    setToggle(previous => !previous)
+  }
+
+  return (
+    <div>
+      {
+        item?.id ? 
+          <>
+            <img src={item.img_url} />
+            <h2>{item.title}</h2>
+            <h3>$ {item.price}</h3>
+            <p>{item.description}</p>
+            
+            {
+              props.currentUser?.id === item.user_id ?
+              <>
+                  <Link to={`/items/${item.id}/edit`}>
+                    <button>Edit</button>
+                  </Link>
+                <button onClick={() => props.handleDelete(item.id)}>Delete</button>
+              </>
+              :
+              null
+            }
+            <BidCreate handleReviewCreate={ handleBidCreate}/>
+            <Bids
+              currentUser={props.currentUser}
+              bids={bids}
+              handleBidDelete={handleBidDelete}
+            />
+          </>
+          :
+          <h3>Sorry, no item found.</h3>
+      }
+    </div>
+  )
+
+}
