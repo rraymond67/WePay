5895ca25f96f37773eece8bd7db4b74fa337a9c4
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-27 22:08:55 -0500

    create bidp

---
+++ client/src/components/BidCreate.jsx
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+
+export default function BidCreate(props) {
+  const [amount, setAmount] = useState(0)
+
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault()
+      const bid = {amount}
+      props.handleBidCreate(bid)
+    }}>
+      <input type='number' value={amount} onChange={(e) => setAmount(e.target.valueAsNumber)} />
+      <button>Place bid!</button>
+    </form>
+  )
+}
