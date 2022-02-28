1545d739e7b965ff0d248c8b731f1df831e56a07
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-27 22:14:53 -0500

    create item

--- client/src/components/ItemCreate.jsx
+++ client/src/components/ItemCreate.jsx
@@ -10,7 +10,30 @@ export default function ItemCreate(props) {
     <form onSubmit={(e) => {
       e.preventDefault()
       const item = { title, description, price, img_url }
-    }}
+      props.handleCreate(item)
+    }}>
+      <input
+        type='text'
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <input
+        type='number'
+        onChange={(e) => setPrice(e.target.valueAsNumber)}
+        value={price}
+      />
+      <input
+        type='text'
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+      />
+      <input
+        type='text'
+        onChange={(e) => setImg_url(e.target.value)}
+        value={img_url}
+      />
+      <button>Create</button>
+    </form>
   )
 
 }
