868a7a58c22e86a8c660a29f4bd84acd76bbb5c1
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-28 08:42:39 -0500

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 import { useEffect, useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
-import {}
+import Register from './components/Register';
+import Login from './components/login';
+import Navbar from './components/Navbar';
+import ItemsContainer from './components/ItemsContainer';
+import { verifyUser } from './services/users';
 
 function App() {
-  return (
-  );
+
+  const [currentUser, setCurrentuser] = useState(null)
+
+  useEffect(()=> {
+    const getUser = async () => {
+      const user = await verifyUser()
+      setCurrentuser(user)
+    }
+    getUser()
+  })
+
+  const logout = async () => {
+    localstorage.removeItem('authToken')
+    setCurrentuser(null)
+  }
+  
 }
 
 export default App;
