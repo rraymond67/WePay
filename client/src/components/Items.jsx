b27b08ef09e55c4150369789742bcc4303105727
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-03-01 15:44:11 -0500

    search bar

--- client/src/components/Items.jsx
+++ client/src/components/Items.jsx
@@ -5,17 +5,22 @@ import Search from "./Search/Search";
 
 export default function Items(props) {
 
+  const [searchResult, setSearchResult] = useState([])
+
   const handleSearch = (e) => {
     const results = props.items.filter((item) =>
       item.title.toLowerCase().includes(e.target.value.toLowerCase())
     )
     setSearchResult(results)
-    setApplySort(true)
   }
+
+  const handleSubmit = (event) => event.preventDefault()
+
   return (
     <div class=" bg-gradient-to-t from-gray-100 to-gray-400">
+      <Search onSubmit={handleSubmit} handleSearch={handleSearch} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center mx-5 drop-shadow-2xl m-2">
-        {props.items.map(item => {
+        {searchResult.map(item => {
           return (
             <div className="m-4 transition ease-in-out delay-150 hover:-translate-y-1 bg-slate-100 hover:scale-105 duration-300  hover:cursor-pointer max-w-sm rounded-2xl overflow-hidden shadow-lg">
           <Link key={item.id} to={`/items/${item.id}`}>
