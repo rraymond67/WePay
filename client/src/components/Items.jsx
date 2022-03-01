0bc6cf3105c7ea5b589bedaeb425068d01fea99a
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-03-01 12:43:15 -0500

    home page

--- client/src/components/Items.jsx
+++ client/src/components/Items.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function Items(props) {
   return (
-    <div className="bg-gradient-to-r from-teal-200 to-teal-500">
+    <div class="bg-gradient-to-t from-gray-100 to-gray-400">
       { props.currentUser &&
-        <Link to='/items/create'>List an Item!</Link>
+        <Link class="py-2 px-3 text-sm font-medium text-center text-slate-800 rounded-lg hover:bg-slate-200 dark:bg-slate-200 dark:hover:bg-slate-400 dark:focus:ring-red-800" to='/items/create'>List an Item!</Link>
       }
       {props.items.map(item => (
         <Link key={item.id} to={`/items/${item.id}`}>
