14390272ab671f54c6e7cb03d1690659fb4bfd71
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-02-28 23:05:50 -0500

    images

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -7,7 +7,7 @@ export default function Login(props) {
   const [password, setPassword] = useState('')
 
   const navigate = useNavigate()
-  
+
   const handleLogin = async (e) => {
     e.preventDefault()
     const user = { username, password }
@@ -18,10 +18,16 @@ export default function Login(props) {
   }
 
   return (
-    <form onSubmit={handleLogin}>
-      <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
-      <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button>Login</button>
+    <div class="px-14 bg-slate-200 grid justify-items-center py-24">
+      <form onSubmit={handleLogin} class="w-full max-w-lg">
+        <div class="w-full px-3 mb-6">
+          <input type='text' placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div class="w-full px-3 mb-6">
+          <input type='password' placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+      <button class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline">Login</button>
     </form>
+    </div>
   )
 }
