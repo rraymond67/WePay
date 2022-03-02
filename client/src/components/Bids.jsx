4ddb23bfa81ec4c1afb6ca0f2060d0e696b80c71
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-03-01 20:08:48 -0500

    search bar

--- client/src/components/Bids.jsx
+++ client/src/components/Bids.jsx
@@ -5,12 +5,12 @@ export default function Bids(props) {
     <div>
       {props.bids && 
         props.bids.map(bid => (
-          <div key={bid.id}>
-            <h4>{bid.user.username} is bidding $ {bid.amount} </h4>
+          <div key={bid.id} class="grid items-center mt-10 grid-cols-2 border-4 bg-green-400 pb-10 mb-10">
+            <h4 class="text-xl text-black-900">{bid.user.username} is bidding $ {bid.amount} </h4>
 
             {props.currentUser?.id === bid.user_id ?
                 <>
-                  <button>Edit</button>
+                
                   <button onClick={() => props.handleBidDelete(bid.id)}>
                     Delete
                   </button>
